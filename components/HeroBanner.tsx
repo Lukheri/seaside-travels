9cdd406728d91f61b396acf0568cb8ceb3fda813
Luke Heri P. Contrivida <88194722+Lukheri@users.noreply.github.com>
2024-02-29 00:41:40 +0800

    Update HeroBanner.tsx

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -33,9 +33,9 @@ const HeroBanner = () => {
             stroke='currentColor'
           >
             <path
-              stroke-linecap='round'
-              stroke-linejoin='round'
-              stroke-width='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
               d='M17 8l4 4m0 0l-4 4m4-4H3'
             />
           </svg>
