c888af3ce60e6f5daf87b5bf1bb164f0ec175eb7
Luke Heri P. Contrivida <88194722+Lukheri@users.noreply.github.com>
2024-02-29 21:28:45 +0800

    Added footer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
-import HeroBanner from '../components/HeroBanner'
+import HeroBanner from '@/components/HeroBanner'
 import About from '@/components/About'
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
     <main className=''>
       <HeroBanner />
       <About />
+      <Footer />
     </main>
   )
 }
