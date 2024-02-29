a6e631278baa770b2808a68ee02d9e5b4e99a110
Luke Heri P. Contrivida <88194722+Lukheri@users.noreply.github.com>
2024-02-29 21:50:13 +0800

    Added Product section

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import HeroBanner from '@/components/HeroBanner'
 import About from '@/components/About'
 import Footer from '@/components/Footer'
+import ProductSection from '@/components/ProductSection'
 
 export default function Home() {
   return (
     <main className=''>
       <HeroBanner />
       <About />
+      <ProductSection />
       <Footer />
     </main>
   )
