9992188deb5d954e179e8749a679ad2a5196a96b
Luke Heri P. Contrivida <88194722+Lukheri@users.noreply.github.com>
2024-03-09 13:44:27 +0800

    Updated Navbar and footer tabs

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,9 @@
-'use client'
 import React, { useCallback, useEffect, useState } from 'react'
 import Announcements from './Announcements'
 import Logo from '@/assets/logo.png'
 import Image from 'next/image'
 
 const Navbar = () => {
-  const scrollToSection = (id: string) => {
-    const element = document.getElementById(id)
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'start' })
-    }
-  }
-
   return (
     <div className='fixed top-0 z-50 flex w-full flex-col bg-primary'>
       <Announcements />
@@ -27,33 +19,33 @@ const Navbar = () => {
           {/* <button className='-px-1 -mb-px inline-flex h-10 items-center whitespace-nowrap border-b-2 border-theme-500 bg-transparent px-2 py-2 text-center text-theme-600 focus:outline-none sm:px-4 '>
               <span className='mx-1 text-sm sm:text-base'>Merch</span>
             </button> */}
-          <button
-            onClick={() => scrollToSection('merch')}
+          <a
+            href='#merch'
             className='-px-1 cursor-base -mb-px inline-flex h-10 items-center whitespace-nowrap border-b-2 border-transparent bg-transparent px-2 py-2 text-center text-gray-700 hover:border-gray-400 focus:outline-none sm:px-4'
           >
             <span className='mx-1 text-sm sm:text-base'>Merch</span>
-          </button>
+          </a>
 
-          <button
-            onClick={() => scrollToSection('events')}
+          <a
+            href='#events'
             className='-px-1 cursor-base -mb-px inline-flex h-10 items-center whitespace-nowrap border-b-2 border-transparent bg-transparent px-2 py-2 text-center text-gray-700 hover:border-gray-400 focus:outline-none sm:px-4'
           >
             <span className='mx-1 text-sm sm:text-base'>Events</span>
-          </button>
+          </a>
 
-          <button
-            onClick={() => scrollToSection('podcast')}
+          <a
+            href='#podcast'
             className='-px-1 cursor-base -mb-px inline-flex h-10 items-center whitespace-nowrap border-b-2 border-transparent bg-transparent px-2 py-2 text-center text-gray-700 hover:border-gray-400 focus:outline-none sm:px-4'
           >
             <span className='mx-1 text-sm sm:text-base'>Podcast</span>
-          </button>
+          </a>
 
-          <button
-            onClick={() => scrollToSection('donate')}
+          <a
+            href='#donate'
             className='-px-1 cursor-base -mb-px inline-flex h-10 items-center whitespace-nowrap border-b-2 border-transparent bg-transparent px-2 py-2 text-center text-gray-700 hover:border-gray-400 focus:outline-none sm:px-4'
           >
             <span className='mx-1 text-sm sm:text-base'>Donate</span>
-          </button>
+          </a>
         </div>
       </div>
     </div>
